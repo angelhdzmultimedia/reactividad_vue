49925c567e4ca98e4b73bb12ad531ad7bbb03da6
Angel Hernandez <angel.hernandez.12@live.com>
2022-06-01 05:51:01 -0400

    asd

--- servidor/src/models/usuario/index.js
+++ servidor/src/models/usuario/index.js
@@ -3,7 +3,7 @@ import { Sequelize, DataTypes, Model } from 'sequelize';
 export const db = new Sequelize('crudDB', 'admin', '123456', {
   host: 'localhost',
   dialect: 'sqlite',
-  storage: `db4.sqlite`,
+  storage: `db.sqlite`,
 });
 
 class Usuario extends Model {}
