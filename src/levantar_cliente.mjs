df45dc496b49a615aeb9ff2a40775bbd0ef223fb
angelhdzmultimedia <angel.hernandez.12@live.com>
2022-06-01 00:35:08 -0400

    2 files modified, 1 added, 1 deleted

--- src/levantar_cliente.mjs
+++ src/levantar_cliente.mjs
@@ -4,8 +4,9 @@ import { exec } from 'child_process';
 const temporizador = setInterval(async () => {
   try {
     const respuesta = await fetch('http://localhost:5000/api');
-    const datos: any = await respuesta.json();
+    const datos = await respuesta.json();
     if (datos.listo) {
+      console.log('Levantando cliente...');
       clearInterval(temporizador);
       exec('npm run start:cliente');
     }
